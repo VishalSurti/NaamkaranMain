aa93fe337830ea7b34ea7076ec18930da9e02cb6
Shaurya Trivedi <shaurya_agent47@yahoo.in>
2018-10-17 14:42:25 +0530

    Shaurya new Commit/ button added

--- app/src/main/java/com/example/shauryatrivedi/naamkaran/MainActivity.java
+++ app/src/main/java/com/example/shauryatrivedi/naamkaran/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.shauryatrivedi.naamkaran;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a,b,c,d;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
